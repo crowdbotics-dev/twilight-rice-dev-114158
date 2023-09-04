3066ace5fba5236f36b8a7e8eef8659ee913383b
Crowdbotics <engineering@crowdbotics.com>
2023-09-04 23:15:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-rice-dev-114158",
+  url: "https://twilight-rice-dev-114158.botics.co",
+  api: "https://twilight-rice-dev-114158.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
